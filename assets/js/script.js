1b03294787cc4141ad82e61c98a9118a25722ed1
Edelgard <jtboyman@gmail.com>
2021-10-21 16:49:23 -0400

    can call question p and answers buttons from arrays and render them on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 let timeLeft = 60;
-let startButton = document.querySelector('#startBtn')
+let startButton = document.querySelector('#startBtn');
+let introBox = document.getElementById('introBox');
+let mainBox = document.getElementById("mainBox");
+const questions = ["Commonly used data types DO NOT include...", "The condition in an if/else statement is enclosed in..."];
+const answers = [
+    {
+        answers1: [
+            {text: "Strings", correct: false},
+            {text: "Booleans", correct: false},
+            {text: "Alerts", correct: true},
+            {text: "Numbers", correct: false},
+        ]
+    },
+]
+
 
 let startTimer = function() {
 
@@ -33,6 +47,50 @@ let startTimer = function() {
 //create 4 button elements and append text
 //on click button will: display 'correct' or 'wrong'; bring up next question
 
-let beginGame
+let beginGame = function() {
+    //removes old box
+    introBox.remove();
+
+    //creates and appends new div to main
+    let questionBoxEl = document.createElement("div");
+    mainBox.appendChild(questionBoxEl);
+
+    //run the function to load question 1
+}
+
+let genQuestion = function() {
+    //create p element for question text and assign variable, append to main
+    let questionText = document.createElement("p");
+    questionText.id = "questionText";
+    mainBox.appendChild(questionText);
+    //pull from array of questions to fill p
+    for (let i = 0; i<questions.length; i++) {
+        questionText.innerText = questions[i];
+        
+    }
+}
+
+let genAnswers = function() {
+    let answer1 = document.createElement("button");
+    mainBox.appendChild(answer1);
+
+    let answer2 = document.createElement("button");
+    mainBox.appendChild(answer2);
+
+    let answer3 = document.createElement("button");
+    mainBox.appendChild(answer3);
+
+    let answer4 = document.createElement("button");
+    mainBox.appendChild(answer4);
+
+    for (let i = 0; i<answers.length; i++) {
+        answer1.innerText = answers[i].answers1[0].text;
+        answer2.innerText = answers[i].answers1[1].text;
+        answer3.innerText = answers[i].answers1[2].text;
+        answer4.innerText = answers[i].answers1[3].text;
+    }
+}
 
 startButton.addEventListener("click", startTimer);
+
+startButton.addEventListener("click", beginGame);
